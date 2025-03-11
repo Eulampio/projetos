65bb75480ba93854badd0c9d724d948622e88bfd
Alexandre Santos <alesantoseulampio@gmail.com>
2025-03-11 17:35:12 -0400

    Update tela_cadastro_time_02.py

--- projetoQT/tela_cadastro_time_02.py
+++ projetoQT/tela_cadastro_time_02.py
@@ -17,6 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QLineEdit,
     QMainWindow, QSizePolicy, QStatusBar, QWidget)
+import image_pasta_times_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -37,17 +38,56 @@ class Ui_MainWindow(object):
         self.times_cadastro.setStyleSheet(u"background-color: rgb(217, 217, 217);")
         self.label_times = QLabel(self.times_cadastro)
         self.label_times.setObjectName(u"label_times")
-        self.label_times.setGeometry(QRect(40, 20, 141, 71))
-        self.lineEdit = QLineEdit(self.times_cadastro)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(30, 100, 631, 51))
-        self.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);")
-        self.lineEdit.setFrame(False)
-        self.lineEdit.setClearButtonEnabled(False)
+        self.label_times.setGeometry(QRect(30, 0, 141, 71))
+        self.cadastrar_time = QLineEdit(self.times_cadastro)
+        self.cadastrar_time.setObjectName(u"cadastrar_time")
+        self.cadastrar_time.setGeometry(QRect(30, 60, 631, 51))
+        self.cadastrar_time.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border: 1px solid white;\n"
+"border-radius: 5px;")
+        self.cadastrar_time.setFrame(False)
+        self.cadastrar_time.setClearButtonEnabled(False)
+        self.label_times_2 = QLabel(self.times_cadastro)
+        self.label_times_2.setObjectName(u"label_times_2")
+        self.label_times_2.setGeometry(QRect(30, 110, 141, 31))
+        self.label_times_3 = QLabel(self.times_cadastro)
+        self.label_times_3.setObjectName(u"label_times_3")
+        self.label_times_3.setGeometry(QRect(30, 150, 641, 51))
+        self.label_times_3.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border: 1px solid white;\n"
+"border-radius: 5px;")
+        self.label_times_3.setPixmap(QPixmap(u":/times/add_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"))
+        self.label_times_3.setScaledContents(False)
+        self.label_times_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label_times_4 = QLabel(self.times_cadastro)
+        self.label_times_4.setObjectName(u"label_times_4")
+        self.label_times_4.setGeometry(QRect(30, 260, 641, 51))
+        self.label_times_4.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border: 1px solid white;\n"
+"border-radius: 5px;")
+        self.label_times_4.setPixmap(QPixmap(u":/times/add_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"))
+        self.label_times_4.setScaledContents(False)
+        self.label_times_4.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label_times_5 = QLabel(self.times_cadastro)
+        self.label_times_5.setObjectName(u"label_times_5")
+        self.label_times_5.setGeometry(QRect(30, 210, 141, 41))
+        self.label_times_6 = QLabel(self.times_cadastro)
+        self.label_times_6.setObjectName(u"label_times_6")
+        self.label_times_6.setGeometry(QRect(40, 320, 141, 31))
+        self.cadastrar_time_2 = QLineEdit(self.times_cadastro)
+        self.cadastrar_time_2.setObjectName(u"cadastrar_time_2")
+        self.cadastrar_time_2.setGeometry(QRect(30, 370, 631, 51))
+        self.cadastrar_time_2.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border: 1px solid white;\n"
+"border-radius: 5px;")
+        self.cadastrar_time_2.setFrame(False)
+        self.cadastrar_time_2.setClearButtonEnabled(False)
         self.widget = QWidget(self.main)
         self.widget.setObjectName(u"widget")
         self.widget.setGeometry(QRect(260, 40, 701, 80))
-        self.widget.setStyleSheet(u"background-color: rgb(143, 191, 131);")
+        self.widget.setStyleSheet(u"background-color: rgb(143, 191, 131);\n"
+"border: 1px solid white;\n"
+"border-radius: 5px;")
         self.time_nome = QLabel(self.widget)
         self.time_nome.setObjectName(u"time_nome")
         self.time_nome.setGeometry(QRect(200, 20, 301, 41))
@@ -70,10 +110,18 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(tooltip)
         self.times_cadastro.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><br/></p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
-        self.label_times.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:14pt;\">Time</span></p></body></html>", None))
-        self.lineEdit.setInputMask("")
-        self.lineEdit.setText("")
-        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Times", None))
+        self.label_times.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:14pt;\">Time 01</span></p></body></html>", None))
+        self.cadastrar_time.setInputMask("")
+        self.cadastrar_time.setText("")
+        self.cadastrar_time.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Times", None))
+        self.label_times_2.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:14pt;\">ESCUDO</span></p></body></html>", None))
+        self.label_times_3.setText("")
+        self.label_times_4.setText("")
+        self.label_times_5.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:14pt;\">Time 02</span></p></body></html>", None))
+        self.label_times_6.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:14pt;\">ESCUDO</span></p></body></html>", None))
+        self.cadastrar_time_2.setInputMask("")
+        self.cadastrar_time_2.setText("")
+        self.cadastrar_time_2.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Times", None))
 #if QT_CONFIG(tooltip)
         self.time_nome.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\">times</p><p align=\"center\"><br/></p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
@@ -83,11 +131,3 @@ class Ui_MainWindow(object):
         self.time_nome.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Times</span></p></body></html>", None))
     # retranslateUi
 
-import sys
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    Form = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec())
